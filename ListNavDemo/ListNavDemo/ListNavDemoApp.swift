4f5c75c2a3c689bb0ccda0cdde8d678a8ca1e85b
Miguel <miguel@miguelchavezweb.com>
2023-06-25 12:38:58 -0400

    Start of ListNavDemo

---
+++ ListNavDemo/ListNavDemo/ListNavDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListNavDemoApp.swift
+//  ListNavDemo
+//
+//  Created by Miguel Chavez on 6/25/23.
+//
+
+import SwiftUI
+
+@main
+struct ListNavDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
